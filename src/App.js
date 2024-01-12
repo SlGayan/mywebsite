3e851104ff7a9953af84c9a3a1de516ed1bf9282
gayan <gayanashoka38@gmail.com>
2024-01-12 23:09:23 +0530

    change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ashoka gayan
         </a>
       </header>
     </div>
