81f7976e779531d57d65d1010009b0d8b06da9de
gayan <gayanashoka38@gmail.com>
2024-01-15 14:47:48 +0530

    we

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import FacebookShare from './/components/facebookshare';
 
 const App = () => {
-  const title = 'My Page Title';
+  const title = 'Sample web site';
   const image = 'https://main.d34lm2jphtlvw6.amplifyapp.com/logo512.png';
   const description = 'This is my page description.';
   const url = 'https://main.d34lm2jphtlvw6.amplifyapp.com/';
 
   return (
-    <div>
-     <div>
+    <div className=''>
+     <div className='App'>
      <img src='../logo512.png'  alt="A beautiful image"  />
      </div>
      <div>
-      <FacebookShare title={title} image={image} description={description} url={url} />
+      <FacebookShare title={title}   url={url} />
     </div>
     </div>
   );
