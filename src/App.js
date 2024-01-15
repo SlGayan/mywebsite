66e632b54f21ab9692985063db9a22119b7f9676
gayan <gayanashoka38@gmail.com>
2024-01-15 11:01:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { Helmet } from 'react-helmet';
-function App() {
+import React from 'react';
+import FacebookShare from './/components/facebookshare';
+
+const App = () => {
+  const title = 'My Page Title';
+  const image = 'https://main.d34lm2jphtlvw6.amplifyapp.com/logo192.png';
+  const description = 'This is my page description.';
+  const url = 'https://main.d34lm2jphtlvw6.amplifyapp.com/';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Helmet>
-          <title>home page</title>
-          <meta property="og:title" content="Your Page Title" />
-        <meta property="og:description" content="Description of your page" />
-        <meta property="og:image" content="https://main.d34lm2jphtlvw6.amplifyapp.com/logo192.png" />
-        <meta property="og:url" content="https://main.d34lm2jphtlvw6.amplifyapp.com/" />
-        <meta property="og:type" content="website" />
-        </Helmet>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p><div>
-        <img src='../logo192.png' className='' />
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          ashoka gayan
-        </a>
-      </header>
+    <div>
+      <FacebookShare title={title} image={image} description={description} url={url} />
     </div>
   );
-}
+};
+
 export default App;
--- src/components/facebookshare.js
+++ src/components/facebookshare.js
@@ -1,40 +1,29 @@
-// ShareImage.js
 import React from 'react';
-import { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 
-const ShareImage = ({ url, title, description, image }) => {
-  useEffect(() => {
-    window. Facebook && window. Facebook.debug && window. Facebook.debug.info();
-  }, []);
-
+const FacebookShare = ({ title, image, description, url }) => {
   return (
     <>
       <Helmet>
         <title>{title}</title>
         <meta property="og:title" content={title} />
-        <meta name="twitter:title" content={title} />
-
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta name="twitter:description" content={description} />
-
         <meta property="og:image" content={image} />
-        <meta name="twitter:image" content={image} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
+        <meta property="og:description" content={description} />
         <meta property="og:url" content={url} />
-        <meta name="twitter:url" content={url} />
-
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <img src={image} alt={title} />
+      <div>
+        <a
+          href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+            url
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Share on Facebook
+        </a>
+      </div>
     </>
   );
 };
 
-export default ShareImage;
+export default FacebookShare;
